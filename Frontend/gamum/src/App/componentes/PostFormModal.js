47474f7f3cc9bab66f530e8d262fd8492d36c7f3
Thiago Silva <thiago.queiroz@sga.pucminas.br>
2020-09-28 15:29:51 -0300

    estilização modal

--- Frontend/gamum/src/App/componentes/PostFormModal.js
+++ Frontend/gamum/src/App/componentes/PostFormModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Modal from 'react-modal'
+import '../styles/modal.css'
 
 
 
@@ -66,15 +67,17 @@ export default class PostFromModal extends React.Component {
               contentLabel= "Selected Option"
               closeTimeoutMS={200}
               ariaHideApp={false}
+              className="modal-container"
             >
-               {this.state.error && <p>{this.state.error}</p>}
+               {this.state.error && <p className="warning">{this.state.error}</p>}
                <form onSubmit={this.onSubmit}>
-                  <input type='text' onChange={this.onTitleChange} placeholder='Digite o título' value={this.state.title}/>
-                  <input type='text' onChange={this.onImgUrlChange} placeholder='Digite a url da imagem' value={this.state.imgUrl}/>
-                  <input type='text' onChange={this.onFontUrlChange} placeholder='Digite a url da fonte' value={this.state.fontUrl}/>
-                  <textarea onChange={this.onContentChange} placeholder='Digite o conteudo da postagem' value = {this.state.content}/>
+                  <h1 className="modal-title">{this.props.title !== ''? 'Crie o seu post' : 'Edite o seu post'}</h1>
+                  <input className="input-form" type='text' onChange={this.onTitleChange} placeholder='Digite o título' value={this.state.title}/>
+                  <input className="input-form" type='text' onChange={this.onImgUrlChange} placeholder='Digite a url da imagem' value={this.state.imgUrl}/>
+                  <input className="input-form" type='text' onChange={this.onFontUrlChange} placeholder='Digite a url da fonte' value={this.state.fontUrl}/>
+                  <textarea className="text-area"onChange={this.onContentChange} placeholder='Digite o conteudo da postagem' value = {this.state.content}/>
 
-                  <button>Add Post</button>
+                  <button className="button">Add Post</button>
                </form>
 
 
