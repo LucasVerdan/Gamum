36df4e9f0affc01af4c46cbcd46c3de177f92bb1
Thiago Silva <thiago.queiroz@sga.pucminas.br>
2020-09-29 10:56:32 -0300

    adição e adaptação template

--- Frontend/gamum/src/App/paginas/UserPosts.js
+++ Frontend/gamum/src/App/paginas/UserPosts.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import CreatePostModal from '../componentes/CreatePostModal';
 import UserPostList from '../componentes/UserPostList'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 
+import Header from '../componentes/Header'
+
+
+
+const sections = [
+    { title: 'Mobile', url: '#' },
+    { title: 'Console', url: '#' },
+    { title: 'PC', url: '#' },
+    { title: 'Virtual Reality', url: '#' },
+
+  ];
 
 const UserPosts = (props) => {
     return (
-        <div>
-           <UserPostList history={props.history}/>
-           <CreatePostModal history={props.history}/>
-        </div>
+        <React.Fragment>
+        <CssBaseline />
+          <Container maxWidth="lg">
+             <Header title="GAMUM" sections={sections} />
+             <UserPostList history={props.history}/>
+             <CreatePostModal history={props.history}/>
+          </Container>
+        </React.Fragment>
     )
 }
 
