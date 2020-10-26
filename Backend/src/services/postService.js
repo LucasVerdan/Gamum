a5bc3fb15b8d7a40f34cc1869bcceec0365934e3
Júlio Bengtson Neves <juliobn1304@gmail.com>
2020-10-25 22:50:22 -0300

    integração parte 2

--- Backend/src/services/postService.js
+++ Backend/src/services/postService.js
@@ -1,9 +1,10 @@
 var Post = require("../models/post");
 var mongoose = require('mongoose');
-const { response } = require("express");
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/gamun";
 
 module.exports.create = async (post) => {
-  const newPost = { ...mongoose.Types.ObjectId, ...post} 
+  const newPost = { ...mongoose.Types.ObjectId, ...post }
   await newPost.save((err) => {
     if (err) console.log(err);
   });
@@ -11,17 +12,13 @@ module.exports.create = async (post) => {
 };
 
 module.exports.getPosts = async () => {
-  console.log('getPostsService')
-  Post.exists().then(res => console.log(res));
-
-    Post.find((err, res) => {
-      console.log(err, res);
-    });
-    console.log('resultado ' + '');
-    return "";
-  };
+  let posts;
+  const client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  var dbo = await client.db("gamun");
+  const result = await dbo.collection("posts").find({}).toArray();
+  return result;
+};
 
 module.exports.deletePost = async (postId) => {
-    Post.findByIdAndDelete(postId, function (err) {});
-  };
-  
+  Post.findByIdAndDelete(postId, function (err) { });
+};
